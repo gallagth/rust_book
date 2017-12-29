eb3f2a460a0735f4c437bd21eaf6eb76357d77c8
Thomas Gallagher <gallagth@gmail.com>
2017-12-29 11:15:07 +0100

    Finished exercism/reverse-string

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,8 +1,3 @@
 pub fn reverse(input: &str) -> String {
-	let chars = input.chars();
-	let mut out = String::with_capacity(input.len());
-	for c in chars.rev() {
-		out.push(c);
-	}
-	out
+	input.chars().rev().collect()
 }
