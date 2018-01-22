8befbbf88b1a36d5580e0d132f7638bd4b4e09ce
Thomas Gallagher <gallagth@gmail.com>
2018-01-22 11:57:27 +0100

    Finished exercism/proverb

---
+++ exercism/rust/proverb/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn build_proverb(list: Vec<&str>) -> String {
+    if list.len() == 0 {
+        return String::new();
+    }
+    let mut out = Vec::new();
+    for i in 0..list.len()-1 {
+        out.push(format!("For want of a {} the {} was lost.", list[i], list[i+1]));
+    }
+    out.push(format!("And all for the want of a {}.", list[0]));
+    return out.join("\n");
+}
